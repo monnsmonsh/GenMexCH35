861287e4419de86e4856d052f21e6a1904e2fd4c
Martín Rodríguez <106488691+monnsmonsh@users.noreply.github.com>
2023-12-11 10:13:18 -0600

    clases js

---
+++ JS/js/script.js
@@ -0,0 +1,68 @@
+//
+//Mensaje de una linea
+//
+
+/*Comentarios
+ */
+
+//alert("hola mundo");
+//console.log("esta es un mensaje")
+
+
+/*var nombre = prompt("Ingresa tu nombre");
+alert("HOLA, " + nombre + "Biemvenido a CH35");
+*/
+
+/*
+var numero1 = parseInt(prompt("Ingresa un numero"));
+var numero2 = parseInt(prompt("Ingresa otro numero"));
+
+console.log("suma:"+(numero1+numero2));
+console.log("resta:"+(numero1-numero2));
+console.log("multiplicacion:"+(numero1*numero2));
+console.log("divicion:"+(numero13/numero2));
+*/
+
+/*
+var nombre = prompt("Ingresa tu nombre");
+var edad = parseInt(prompt("Cual es tu edad"));
+
+if (edad>40) {
+    alert("HOLA, " + nombre + " Ya tas viejo");
+    } else if(edad>30) {
+    alert("HOLA, " + nombre + " Eres un chavo ruco >.<");
+    } else if(edad>=18) {
+        alert("HOLA, " + nombre + " ya eres adulto 3:)");
+    }else if(edad<18){
+        alert("HOLA, " + nombre + " eres un chavo");
+    }
+*/
+
+
+
+
+/*
+--------------------------------------------------------
+08- DICIEMBRE
+--------------------------------------------------------
+*/
+
+//tipos de datos y variables
+/*var, let, const la diferencia es el alcance
+var tiene un alcance global
+let tiene un alcnace local
+const se mantiene sin ningun cambio*/
+
+// string: cadena de texto (lleva comillas)
+// number: numero
+// noolena: true/false
+// null : nulo
+// undefinied : no esta asigando o definido
+
+/*
+let nombre = "Daniel";
+let invitadxsExtras = 2;
+let esMayorEdad = true;
+let edad = 24;
+*/
+

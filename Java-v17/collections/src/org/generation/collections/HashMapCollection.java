c774c013bae418c94902da40a15c4b908372c341
Martín Rodríguez <monse.developer@gmail.com>
2024-01-19 16:52:39 -0600

    Enero 19 16:52hrs

---
+++ Java-v17/collections/src/org/generation/collections/HashMapCollection.java
@@ -0,0 +1,46 @@
+package org.generation.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapCollection {
+
+	public static void main(String[] args) {
+		/**
+		 * HashMap almacena elementos en pares "clave/valor" y se puede acceder a ellos mediante index.
+		 * No es ordenado y permite datos duplicados
+		 */
+		
+		HashMap<String, String> capitals = new HashMap<String, String>();
+		
+		//Add -> Agregando elementos "clave/valor" (.put)
+		capitals.put("Villahermosa "," Tabasco");
+		capitals.put("Monterrey "," Nuevo Leon");
+		capitals.put("Guadalajara "," Jalisco");
+		capitals.put("Merida "," Yucatan");
+		capitals.put("Toluca "," Estado de Mexico");
+		
+		System.out.println(capitals);
+		
+		//Map.Entry<> es un método de la interfaz Map que representa una entrada en un Map. Toma como argumento los tipos de datos asignados al HashMap. El usuario asigna el nombre de la interfaz y recibe un método .entrySet() que devuelve el conjunto (Set) de las entrada (clave-valor) en el HashMap.
+		//De esta manera ya podemos utilizar los métodos de abajo
+		for(Map.Entry<String, String> entry: capitals.entrySet()) {
+			//Mostrar la clave y el valor de manera individual para su manipulacion
+			System.out.println(entry.getKey()+" es la capital de "+ entry.getValue());
+		}
+		
+		//************* Ejemplo 02 *************//
+		Map<String, Integer> calificaciones = new HashMap<String, Integer>();
+		calificaciones.put("Rene", 9);
+		calificaciones.put("Ximena", 10);
+		calificaciones.put("Diego", 8);
+		calificaciones.put("Eunice", 6);
+		calificaciones.put("Eli", 7);
+		
+		for(Map.Entry<String, Integer> prom: calificaciones.entrySet()) {
+			System.out.println("La calificacion de "+prom.getKey()+" es de "+ prom.getValue());
+		}
+		
+	}
+
+}
---
+++ Java-v17/collections/src/org/generation/exercises/Productos.java
@@ -0,0 +1,10 @@
+package org.generation.exercises;
+
+public class Productos {
+	/*
+	 * Crear un programa que le permita al usuario agregar un id de producto y el nombre del producto. Mostrar la lista de productos.
+	 * */
+	public static void main(String[] args) {
+		
+	}
+}

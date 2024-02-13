825d728361db6be9f7e79df7d359df5cba861379
Martín Rodríguez <monse.developer@gmail.com>
2024-02-13 12:41:13 -0600

    Update UserController.java

--- Java-v17/umbrellaUser/src/main/java/org/generation/umbrellaUser/controller/UserController.java
+++ Java-v17/umbrellaUser/src/main/java/org/generation/umbrellaUser/controller/UserController.java
@@ -7,6 +7,7 @@ import org.generation.umbrellaUser.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,12 +15,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController //Indica que esta clase es una API de tipo REST
 @RequestMapping("admin") //Contruimos la ruta de nuesta API, def endpoints (localhost:8080/admin/users)
+@CrossOrigin(origins="*", methods={RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class UserController {
 	
 	private UserService userService;
